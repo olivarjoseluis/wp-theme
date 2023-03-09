41e18292dff40f47cdc09b7b3ea9ad4fecbe00a5
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-03-09 16:54:03 -0500

    Initial commit

---
+++ app/public/wp-content/themes/yardsales/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center my-4">
+  De LATAM con ⚽ para el mundo
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ app/public/wp-content/themes/yardsales/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+function store_assets()
+{
+  wp_register_style("google_fonts", "https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700", array(), false, 'all');
+  wp_register_style("google_fonts_2", "https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap", array(), false, 'all');
+  wp_register_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css", array(), "5-1", 'all');
+
+  wp_enqueue_style("styles", get_template_directory_uri() . "/assets/css/style.css", array("google_fonts", "bootstrap"));
+
+  wp_enqueue_script("yard_sale_js", get_template_directory_uri() . "/assets/js/script.js");
+}
+
+add_action("wp_enqueue_scripts", "store_assets");
+
+
+function store_theme_supports()
+{
+  add_theme_support("title-tag");
+  add_theme_support("post-thumbnails");
+  add_theme_support("custom-logo", array("width" => 170, "height" => 35, "flex-width" => true, "flex-height" => true));
+}
+
+add_action("after_setup_theme", "store_theme_supports");
